SoftwareHeritage/add-forge-now-requests

commit 2e3bf62a79be4872cacd216129c9933182286842

Add gitlab-ci components

.gitlab-ci/bash-functions.sh
@@ -1,0 +1,108 @@
+set -euo pipefail
+
+check_network_ports () {
+    for url in $STAGING_SCHEDULER_URL $PRODUCTION_SCHEDULER_URL $AMQP_URL; do
+        echo -e "\n# ${url} #"; curl -sI "$url"
+    done
+    echo -e "\n# amqp.local 5672 #"
+    nc -zv amqp.local 5672
+    echo -e "\n# https://${INSTANCE_NAME} #"
+    curl -sI --connect-timeout 5 https://"${INSTANCE_NAME}"./ | grep '^HTTP.*\(2..\|3..\)'
+}
+
+gitlab_close_issue () {
+    curl -s -X PUT -H "PRIVATE-TOKEN: ${ADD_FORGE_NOW_ISSUE_TOKEN}" \
+    "${GITLAB_URL}/api/v4/projects/${PROJECT_ID}/issues/${ISSUE_ID}?state_event=close"
+}
+
+gitlab_commit_readme_history () {
+    trap "rm README.new" EXIT
+    local STATUS=$1
+    local VISIT_DATE=$(date "+%F %T %z%Z")
+    curl -s -H "PRIVATE_TOKEN: ${ADD_FORGE_NOW_ISSUE_TOKEN}" \
+    "${GITLAB_URL}/api/v4/projects/${PROJECT_ID}/repository/files/README.md?ref=main" | \
+    jq -r .content | base64 -d > README.new
+    echo "| ${INSTANCE_NAME} | ${LISTER_TYPE} | ${STATUS} | ${VISIT_DATE} | #${ISSUE_ID} |" >> README.new
+    curl -s -X POST \
+     -F "branch=main" \
+     -F "commit_message=afnr: 🤖 process ${STATUS}ly ${INSTANCE_NAME}" \
+     -F "actions[][action]=update" \
+     -F "actions[][file_path]=README.md" \
+     -F "actions[][content]=<README.new" \
+     -H "PRIVATE-TOKEN: ${ADD_FORGE_NOW_ISSUE_TOKEN}" \
+    "${GITLAB_URL}/api/v4/projects/${PROJECT_ID}/repository/commits"
+}
+
+gitlab_create_issue () {
+ISSUE_ID=$(curl -s -X POST -H "PRIVATE-TOKEN: ${ADD_FORGE_NOW_ISSUE_TOKEN}" \
+"${GITLAB_URL}/api/v4/projects/${PROJECT_ID}/issues?\
+title=%5BAdd%20Forge%20Now%5D%20process%20https%3A%2F%2F${INSTANCE_NAME}&\
+labels=AddForgeNow&description=${WEBAPP_URL}/admin/add-forge/request/${REQUEST_ID}/\
+%20%20%0D%0AType%3A%20${LISTER_TYPE}&\
+milestone_id=${MILESTONE_ID}" \
+| jq '.iid')
+export ISSUE_ID=$ISSUE_ID
+}
+
+gitlab_update_issue () {
+    local STATUS=$1
+    local MID_MSG="%20in%20${ENV}%20environment%2E%20%20%0D%0A"
+    local END_MSG="For%20the%20pipeline%20details%2C%20see%3A%20%20%0D%0A${CI_PIPELINE_URL}%2E"
+    local SUCCESS_MSG="Request%20successfully%20processed"
+    local FAILED_MSG="Request%20completly%20messed%20up"
+    [ "$STATUS" = "success" ] && local COMMENT="${SUCCESS_MSG}${MID_MSG}${END_MSG}"
+    [ "$STATUS" = "failure" ] && local COMMENT="${FAILED_MSG}${MID_MSG}${END_MSG}"
+    curl -s -X POST -H "PRIVATE-TOKEN: ${ADD_FORGE_NOW_ISSUE_TOKEN}" \
+    "${GITLAB_URL}/api/v4/projects/${PROJECT_ID}/issues/${ISSUE_ID}/notes?body=${COMMENT}"
+}
+
+register_vars () {
+    local ENV=$1
+    local SCHEDULER_URL=$2
+    echo "ENV=$ENV" > build.env
+    echo "SCHEDULER_URL=$SCHEDULER_URL" >> build.env
+    echo "ISSUE_ID=$ISSUE_ID" >> build.env
+}
+
+scheduler_check_ingested_origins () {
+    swh scheduler --url "$SCHEDULER_URL" \
+    origin check-ingested-origins -l -w \
+    "$LISTER_TYPE" "$INSTANCE_NAME"
+}
+
+scheduler_check_listed_origins () {
+    swh scheduler --url "$SCHEDULER_URL" \
+    origin check-listed-origins -l \
+    "$LISTER_TYPE" "$INSTANCE_NAME"
+}
+
+scheduler_register_lister () {
+    swh scheduler --url "$SCHEDULER_URL" \
+    add-forge-now --preset "$ENV" \
+    register-lister "$LISTER_TYPE" \
+    instance="$INSTANCE_NAME"
+    sleep "$LISTING_DELAY"
+}
+
+scheduler_schedule_first_visits () {
+    swh scheduler --config-file "$SWH_CONFIG_FILENAME" \
+    --url "$SCHEDULER_URL" \
+    add-forge-now --preset "$ENV" \
+    schedule-first-visits \
+    --type-name git \
+    --lister-name "$LISTER_TYPE" \
+    --lister-instance-name "$INSTANCE_NAME"
+}
+
+webapp_comment_and_status () {
+    local COMMENT="For details, see ${GITLAB_URL}/infra/${PROJECT_NAME}/-/issues/${ISSUE_ID}."
+    if [ "$#" -eq 0 ]; then
+        local TEXT="${COMMENT}"
+    elif [ "$#" -eq 1 ]; then
+        local TEXT="${COMMENT}\",\"new_status\":\"${1}"
+    fi
+    curl -s -H 'Content-Type: application/json' \
+    -H "Authorization: Bearer $WEBAPP_TOKEN" \
+    -d "{\"text\":\"${TEXT}\"}" \
+    "${WEBAPP_URL}/api/1/add-forge/request/${REQUEST_ID}/update/"
+}

.gitlab-ci.yml
@@ -1,0 +1,188 @@
+image:
+  name: 192.168.49.2:5000/add-forge-now:20230907.4
+
+default:
+  before_script:
+    - if [ -f build.env ]; then source build.env; fi
+    - source .gitlab-ci/bash-functions.sh
+  tags:
+    - AddForgeNowRequest
+
+variables:
+  STAGING_SCHEDULER_URL: http://scheduler.local:5008
+  PRODUCTION_SCHEDULER_URL: http://saatchi.local:5008
+  AMQP_URL: http://amqp.local:15672
+  GITLAB_URL: http://gitlab.local
+  WEBAPP_URL: http://webapp.local:5080
+  LISTING_DELAY: 20
+  PROJECT_ID: 1
+  PROJECT_NAME: add-forge-now-requests
+  MILESTONE_ID: 1
+
+# STAGES
+
+stages:
+  - staging_list_origins
+  - staging_ingest_origins
+  - staging_set_status
+  #- production_list-origins
+  #- production_ingest-origins
+  - production_set_status
+
+# TEMPLATES
+
+.staging_status:
+  artifacts:
+    public: false
+    untracked: true
+    expire_in: "7 days"
+    paths:
+      - "build.env"
+  dependencies:
+    - "check_and_prepare"
+
+.on_success_status:
+  rules:
+    - if: $CI_PIPELINE_SOURCE == "trigger"
+      when: on_success
+
+.on_failure_status:
+  rules:
+    - if: $CI_PIPELINE_SOURCE == "trigger"
+      when: on_failure
+
+# JOBS
+
+check_and_prepare:
+  stage: .pre
+  script:
+    - gitlab_create_issue
+    - webapp_comment_and_status
+    - check_network_ports
+    - register_vars staging "$STAGING_SCHEDULER_URL"
+  artifacts:
+    public: false
+    untracked: true
+    expire_in: "7 days"
+    paths:
+      - "build.env"
+  rules:
+    - if: $CI_PIPELINE_SOURCE == "trigger"
+
+1s_register_lister:
+  stage: staging_list_origins
+  script:
+    - scheduler_register_lister
+  extends:
+    - .on_success_status
+  dependencies:
+    - "check_and_prepare"
+
+2s_check_listed_origins:
+  stage: staging_list_origins
+  script:
+    - scheduler_check_listed_origins
+  extends:
+    - .on_success_status
+  needs:
+    - job: check_and_prepare
+      artifacts: true
+    - job: 1s_register_lister
+
+3s_schedule_first_visits:
+  stage: staging_ingest_origins
+  script:
+    - scheduler_schedule_first_visits
+  extends:
+    - .on_success_status
+
+4s_check_ingested_origins:
+  stage: staging_ingest_origins
+  script:
+    - scheduler_check_ingested_origins
+  extends:
+    - .on_success_status
+  needs:
+    - job: check_and_prepare
+      artifacts: true
+    - job: 3s_schedule_first_visits
+
+staging_status_successful:
+  stage: staging_set_status
+  extends:
+    - .staging_status
+    - .on_success_status
+  script:
+    - gitlab_update_issue success
+    - webapp_comment_and_status SCHEDULED
+    - register_vars production "$PRODUCTION_SCHEDULER_URL"
+
+staging_status_unsuccessful:
+  stage: staging_set_status
+  extends:
+    - .staging_status
+    - .on_failure_status
+  script:
+    - gitlab_update_issue failure
+    - register_vars production "$PRODUCTION_SCHEDULER_URL"
+
+#1p_register-lister:
+#  stage: production_list-origins
+#  script:
+#    - scheduler_register_lister
+#  extends:
+#    - .on_success_status
+#  dependencies:
+#    - "staging_status_successful"
+#
+#2p_check-listed-origins:
+#  stage: production_list-origins
+#  script:
+#    - scheduler_check_listed_origins
+#  extends:
+#    - .on_success_status
+#  needs:
+#    - job: staging_status_successful
+#      artifacts: true
+#    - job: 1p_register-lister
+#
+#3p_schedule-first-visits:
+#  stage: production_ingest-origins
+#  script:
+#    - scheduler_schedule_first_visits
+#  extends:
+#    - .on_success_status
+#
+#4p_check-ingested-origins:
+#  stage: production_ingest-origins
+#  script:
+#    - scheduler_check_ingested_origins
+#  extends:
+#    - .on_success_status
+#  needs:
+#    - job: staging_status_successful
+#      artifacts: true
+#    - job: 3p_schedule-first-visits
+#
+production_status_successful:
+  stage: production_set_status
+  extends:
+    - .on_success_status
+  script:
+    - gitlab_update_issue success
+    - gitlab_close_issue
+    - webapp_comment_and_status FIRST_ORIGIN_LOADED
+    - gitlab_commit_readme_history successful
+  dependencies:
+    - "staging_status_successful"
+
+production_status_unsuccessful:
+  stage: production_set_status
+  extends:
+    - .on_failure_status
+  script:
+    - gitlab_update_issue failure
+    - webapp_comment_and_status UNSUCCESSFUL
+    - gitlab_commit_readme_history unsuccessful
+  dependencies:
+    - "staging_status_unsuccessful"

commit 2b7eb381d5c822f848e2800393d42494a4c172f8

Fix typo
Fixes: commit 2e3bf62a79be4872cacd216129c9933182286842

.gitlab-ci/bash-functions.sh
@@ -72,7 +72,7 @@ gitlab_update_issue () {
     local END_MSG="For the pipeline details, see:  ${EOL}"
     END_MSG+="${CI_PIPELINE_URL}."
     local SUCCESS_MSG="Request successfully processed"
-    local FAILED_MSG="Request completly messed up"
+    local FAILED_MSG="Request completely messed up"
     [ "$STATUS" = "success" ] && local -x COMMENT="${SUCCESS_MSG}${MID_MSG}${END_MSG}"
     [ "$STATUS" = "failure" ] && local -x COMMENT="${FAILED_MSG}${MID_MSG}${END_MSG}"
     curl -s -X POST -H "PRIVATE-TOKEN: ${ADD_FORGE_NOW_ISSUE_TOKEN}" \